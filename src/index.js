ae5b4b2e242105a7b365a04e8cfd6f0d07bde4bc
nadinaaa <43186448+nadinaaa@users.noreply.github.com>
2018-09-28 12:29:34 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,3 +1,14 @@
 module.exports = function getZerosCount(number) {
-  // your implementation
+  let k=0,l=0,p=0,i=0;
+  for(i=1;i<=number;i++)
+{
+  p=i;
+  l=0;
+  while(p%5==0){
+    l++;
+    p/=5;
+  }
+  k+=l;
+}
+return k;
 }
